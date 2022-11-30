c285d205cab65490837d3dfaffdc373bc2bd9e08
Pascal <michelpa@gmail.com>
2022-11-30 10:09:03 +0100

    [SD] remove unused methods

--- src/VarioSD/VarioSD.h
+++ src/VarioSD/VarioSD.h
@@ -13,18 +13,6 @@ public:
     void end(void);
     //   void printDirectory();
     //   void printAllDirectory(File dir, int numTabs);
-    bool info(void);
-    void test(void);
-
-    void listDir(fs::FS &fs, const char *dirname, uint8_t levels);
-    void createDir(fs::FS &fs, const char *path);
-    void removeDir(fs::FS &fs, const char *path);
-    void readFile(fs::FS &fs, const char *path);
-    void writeFile(fs::FS &fs, const char *path, const char *message);
-    void appendFile(fs::FS &fs, const char *path, const char *message);
-    void renameFile(fs::FS &fs, const char *path1, const char *path2);
-    void deleteFile(fs::FS &fs, const char *path);
-    void testFileIO(fs::FS &fs, const char *path);
 
 private:
     SPIClass spiSdCard{HSPI};
