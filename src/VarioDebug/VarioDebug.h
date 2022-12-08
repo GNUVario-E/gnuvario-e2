72cb538f3bd4bc9c69415bff430baaa2aa6e7738
nstCactus <nstCactus@gmail.com>
2022-12-08 02:24:26 +0100

    style: group preprocessor declarations in VarioDebug.h

--- src/VarioDebug/VarioDebug.h
+++ src/VarioDebug/VarioDebug.h
@@ -45,6 +45,14 @@
 #define VARIO_PROG_DEBUG_DUMP(x)
 #define VARIO_PROG_DEBUG_DUMP2(x, y)
 #define VARIO_PROG_DEBUG_TRACE()
+#else
+#define VARIO_PROG_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_PROG_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_PROG_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_PROG_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_PROG_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_PROG_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_PROG_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(IMU_DEBUG)
@@ -55,6 +63,14 @@
 #define VARIO_IMU_DEBUG_DUMP(x)
 #define VARIO_IMU_DEBUG_DUMP2(x, y)
 #define VARIO_IMU_DEBUG_TRACE()
+#else
+#define VARIO_IMU_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_IMU_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_IMU_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_IMU_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_IMU_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_IMU_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_IMU_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(BTN_DEBUG)
@@ -65,6 +81,14 @@
 #define VARIO_BTN_DEBUG_DUMP(x)
 #define VARIO_BTN_DEBUG_DUMP2(x, y)
 #define VARIO_BTN_DEBUG_TRACE()
+#else
+#define VARIO_BTN_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_BTN_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_BTN_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_BTN_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_BTN_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_BTN_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_BTN_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(SDCARD_DEBUG)
@@ -75,6 +99,14 @@
 #define VARIO_SDCARD_DEBUG_DUMP(x)
 #define VARIO_SDCARD_DEBUG_DUMP2(x, y)
 #define VARIO_SDCARD_DEBUG_TRACE()
+#else
+#define VARIO_SDCARD_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_SDCARD_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_SDCARD_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_SDCARD_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_SDCARD_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_SDCARD_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_SDCARD_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(SOUND_DEBUG)
@@ -85,6 +117,14 @@
 #define VARIO_SOUND_DEBUG_DUMP(x)
 #define VARIO_SOUND_DEBUG_DUMP2(x, y)
 #define VARIO_SOUND_DEBUG_TRACE()
+#else
+#define VARIO_SOUND_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_SOUND_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_SOUND_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_SOUND_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_SOUND_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_SOUND_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_SOUND_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(SQL_DEBUG)
@@ -95,6 +135,14 @@
 #define VARIO_SQL_DEBUG_DUMP(x)
 #define VARIO_SQL_DEBUG_DUMP2(x, y)
 #define VARIO_SQL_DEBUG_TRACE()
+#else
+#define VARIO_SQL_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_SQL_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_SQL_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_SQL_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_SQL_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_SQL_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_SQL_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(MEMORY_DEBUG)
@@ -105,6 +153,14 @@
 #define VARIO_MEMORY_DEBUG_DUMP(x)
 #define VARIO_MEMORY_DEBUG_DUMP2(x, y)
 #define VARIO_MEMORY_DEBUG_TRACE()
+#else
+#define VARIO_MEMORY_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_MEMORY_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_MEMORY_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_MEMORY_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_MEMORY_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_MEMORY_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_MEMORY_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(CAL_DEBUG)
@@ -115,6 +171,14 @@
 #define VARIO_CAL_DEBUG_DUMP(x)
 #define VARIO_CAL_DEBUG_DUMP2(x, y)
 #define VARIO_CAL_DEBUG_TRACE()
+#else
+#define VARIO_CAL_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_CAL_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_CAL_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_CAL_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_CAL_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_CAL_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_CAL_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(WIFI_DEBUG)
@@ -125,6 +189,14 @@
 #define VARIO_WIFI_DEBUG_DUMP(x)
 #define VARIO_WIFI_DEBUG_DUMP2(x, y)
 #define VARIO_WIFI_DEBUG_TRACE()
+#else
+#define VARIO_WIFI_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_WIFI_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_WIFI_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_WIFI_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_WIFI_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_WIFI_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_WIFI_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(DATA_DEBUG)
@@ -135,6 +207,14 @@
 #define VARIO_DATA_DEBUG_DUMP(x)
 #define VARIO_DATA_DEBUG_DUMP2(x, y)
 #define VARIO_DATA_DEBUG_TRACE()
+#else
+#define VARIO_DATA_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_DATA_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_DATA_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_DATA_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_DATA_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_DATA_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_DATA_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(GPS_DEBUG)
@@ -145,6 +225,14 @@
 #define VARIO_GPS_DEBUG_DUMP(x)
 #define VARIO_GPS_DEBUG_DUMP2(x, y)
 #define VARIO_GPS_DEBUG_TRACE()
+#else
+#define VARIO_GPS_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_GPS_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_GPS_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_GPS_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_GPS_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_GPS_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_GPS_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(NMEAPARSER_DEBUG)
@@ -155,6 +243,14 @@
 #define VARIO_NMEAPARSER_DEBUG_DUMP(x)
 #define VARIO_NMEAPARSER_DEBUG_DUMP2(x, y)
 #define VARIO_NMEAPARSER_DEBUG_TRACE()
+#else
+#define VARIO_NMEAPARSER_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_NMEAPARSER_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_NMEAPARSER_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_NMEAPARSER_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_NMEAPARSER_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_NMEAPARSER_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_NMEAPARSER_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(FSM_DEBUG)
@@ -165,16 +261,14 @@
 #define VARIO_FSM_DEBUG_DUMP(x)
 #define VARIO_FSM_DEBUG_DUMP2(x, y)
 #define VARIO_FSM_DEBUG_TRACE()
-#endif
-
-#if !defined(VARIO_DEBUG) || !defined(GPS_DEBUG)
-#define VARIO_GPS_DEBUG_PRINTLN(x)
-#define VARIO_GPS_DEBUG_PRINTLN2(x, y)
-#define VARIO_GPS_DEBUG_PRINT(x)
-#define VARIO_GPS_DEBUG_PRINT2(x, y)
-#define VARIO_GPS_DEBUG_DUMP(x)
-#define VARIO_GPS_DEBUG_DUMP2(x, y)
-#define VARIO_GPS_DEBUG_TRACE()
+#else
+#define VARIO_FSM_DEBUG_PRINTLN(x) Serial.println(x)
+#define VARIO_FSM_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
+#define VARIO_FSM_DEBUG_PRINT(x) Serial.print(x)
+#define VARIO_FSM_DEBUG_PRINT2(x, y) Serial.print(x, y)
+#define VARIO_FSM_DEBUG_DUMP(x) DUMP(x)
+#define VARIO_FSM_DEBUG_DUMP2(x, y) DUMP2(x, y)
+#define VARIO_FSM_DEBUG_TRACE() TRACE()
 #endif
 
 #if !defined(VARIO_DEBUG) || !defined(AGL_DEBUG)
@@ -185,180 +279,7 @@
 #define VARIO_AGL_DEBUG_DUMP(x)
 #define VARIO_AGL_DEBUG_DUMP2(x, y)
 #define VARIO_AGL_DEBUG_TRACE()
-#endif
-
-#if !defined(VARIO_DEBUG) || !defined(IGC_DEBUG)
-#define VARIO_IGC_DEBUG_PRINTLN(x)
-#define VARIO_IGC_DEBUG_PRINTLN2(x, y)
-#define VARIO_IGC_DEBUG_PRINT(x)
-#define VARIO_IGC_DEBUG_PRINT2(x, y)
-#define VARIO_IGC_DEBUG_DUMP(x)
-#define VARIO_IGC_DEBUG_DUMP2(x, y)
-#define VARIO_IGC_DEBUG_TRACE()
-#endif
-
-#if !defined(VARIO_DEBUG) || !defined(PARAM_DEBUG)
-#define VARIO_PARAM_DEBUG_PRINTLN(x)
-#define VARIO_PARAM_DEBUG_PRINTLN2(x, y)
-#define VARIO_PARAM_DEBUG_PRINT(x)
-#define VARIO_PARAM_DEBUG_PRINT2(x, y)
-#define VARIO_PARAM_DEBUG_DUMP(x)
-#define VARIO_PARAM_DEBUG_DUMP2(x, y)
-#define VARIO_PARAM_DEBUG_TRACE()
-#endif
-
-#if !defined(VARIO_DEBUG) || !defined(PREFS_DEBUG)
-#define VARIO_PREFS_DEBUG_PRINTLN(x)
-#define VARIO_PREFS_DEBUG_PRINTLN2(x, y)
-#define VARIO_PREFS_DEBUG_PRINT(x)
-#define VARIO_PREFS_DEBUG_PRINT2(x, y)
-#define VARIO_PREFS_DEBUG_DUMP(x)
-#define VARIO_PREFS_DEBUG_DUMP2(x, y)
-#define VARIO_PREFS_DEBUG_TRACE()
-#endif
-/*************************************************/
-
-#if defined(VARIO_DEBUG) && defined(PROG_DEBUG)
-#define VARIO_PROG_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_PROG_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_PROG_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_PROG_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_PROG_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_PROG_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_PROG_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(IMU_DEBUG)
-#define VARIO_IMU_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_IMU_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_IMU_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_IMU_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_IMU_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_IMU_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_IMU_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(BTN_DEBUG)
-#define VARIO_BTN_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_BTN_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_BTN_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_BTN_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_BTN_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_BTN_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_BTN_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(SDCARD_DEBUG)
-#define VARIO_SDCARD_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_SDCARD_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_SDCARD_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_SDCARD_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_SDCARD_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_SDCARD_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_SDCARD_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(SOUND_DEBUG)
-#define VARIO_SOUND_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_SOUND_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_SOUND_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_SOUND_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_SOUND_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_SOUND_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_SOUND_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(SQL_DEBUG)
-#define VARIO_SQL_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_SQL_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_SQL_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_SQL_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_SQL_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_SQL_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_SQL_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(MEMORY_DEBUG)
-#define VARIO_MEMORY_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_MEMORY_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_MEMORY_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_MEMORY_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_MEMORY_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_MEMORY_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_MEMORY_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(CAL_DEBUG)
-#define VARIO_CAL_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_CAL_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_CAL_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_CAL_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_CAL_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_CAL_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_CAL_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(WIFI_DEBUG)
-#define VARIO_WIFI_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_WIFI_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_WIFI_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_WIFI_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_WIFI_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_WIFI_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_WIFI_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(DATA_DEBUG)
-#define VARIO_DATA_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_DATA_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_DATA_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_DATA_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_DATA_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_DATA_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_DATA_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(GPS_DEBUG)
-#define VARIO_GPS_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_GPS_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_GPS_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_GPS_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_GPS_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_GPS_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_GPS_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(NMEAPARSER_DEBUG)
-#define VARIO_NMEAPARSER_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_NMEAPARSER_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_NMEAPARSER_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_NMEAPARSER_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_NMEAPARSER_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_NMEAPARSER_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_NMEAPARSER_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(FSM_DEBUG)
-#define VARIO_FSM_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_FSM_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_FSM_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_FSM_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_FSM_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_FSM_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_FSM_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(GPS_DEBUG)
-#define VARIO_GPS_DEBUG_PRINTLN(x) Serial.println(x)
-#define VARIO_GPS_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_GPS_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_GPS_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_GPS_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_GPS_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_GPS_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(AGL_DEBUG)
+#else
 #define VARIO_AGL_DEBUG_PRINTLN(x) Serial.println(x);
 #define VARIO_AGL_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
 #define VARIO_AGL_DEBUG_PRINT(x) Serial.print(x)
@@ -376,6 +297,14 @@
 #define VARIO_IGC_DEBUG_DUMP(x) DUMP(x)
 #define VARIO_IGC_DEBUG_DUMP2(x, y) DUMP2(x, y)
 #define VARIO_IGC_DEBUG_TRACE() TRACE()
+#else
+#define VARIO_IGC_DEBUG_PRINTLN(x)
+#define VARIO_IGC_DEBUG_PRINTLN2(x, y)
+#define VARIO_IGC_DEBUG_PRINT(x)
+#define VARIO_IGC_DEBUG_PRINT2(x, y)
+#define VARIO_IGC_DEBUG_DUMP(x)
+#define VARIO_IGC_DEBUG_DUMP2(x, y)
+#define VARIO_IGC_DEBUG_TRACE()
 #endif
 
 #if defined(VARIO_DEBUG) && defined(PARAM_DEBUG)
@@ -386,14 +315,12 @@
 #define VARIO_PARAM_DEBUG_DUMP(x) DUMP(x)
 #define VARIO_PARAM_DEBUG_DUMP2(x, y) DUMP2(x, y)
 #define VARIO_PARAM_DEBUG_TRACE() TRACE()
-#endif
-
-#if defined(VARIO_DEBUG) && defined(PREFS_DEBUG)
-#define VARIO_PREFS_DEBUG_PRINTLN(x) Serial.println(x);
-#define VARIO_PREFS_DEBUG_PRINTLN2(x, y) Serial.println(x, y)
-#define VARIO_PREFS_DEBUG_PRINT(x) Serial.print(x)
-#define VARIO_PREFS_DEBUG_PRINT2(x, y) Serial.print(x, y)
-#define VARIO_PREFS_DEBUG_DUMP(x) DUMP(x)
-#define VARIO_PREFS_DEBUG_DUMP2(x, y) DUMP2(x, y)
-#define VARIO_PREFS_DEBUG_TRACE() TRACE()
+#else
+#define VARIO_PARAM_DEBUG_PRINTLN(x)
+#define VARIO_PARAM_DEBUG_PRINTLN2(x, y)
+#define VARIO_PARAM_DEBUG_PRINT(x)
+#define VARIO_PARAM_DEBUG_PRINT2(x, y)
+#define VARIO_PARAM_DEBUG_DUMP(x)
+#define VARIO_PARAM_DEBUG_DUMP2(x, y)
+#define VARIO_PARAM_DEBUG_TRACE()
 #endif
