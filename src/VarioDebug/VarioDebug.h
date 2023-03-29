20ed83c2bf20bbc99279ce04ed530d0fb7b5f1cc
Pascal <michelpa@gmail.com>
2023-03-29 10:03:33 +0200

    [Wifi] Fix params reading and optimize memory

--- src/VarioDebug/VarioDebug.h
+++ src/VarioDebug/VarioDebug.h
@@ -46,15 +46,15 @@ extern SdDebug sdDebug;
 // #define IGC_DEBUG
 // #define DATA_DEBUG
 // #define BT_DEBUG
-// #define WIFI_DEBUG
+#define WIFI_DEBUG
 // #define SOUND_DEBUG
 // #define AGL_DEBUG
 // #define SQL_DEBUG
 // #define BEARING_DEBUG
 // #define TWOWIRESCH_DEBUG
 // #define POWER_DEBUG
-// #define MEMORY_DEBUG
-#define FSM_DEBUG
+#define MEMORY_DEBUG
+// #define FSM_DEBUG
 // #define PARAM_DEBUG
 // #define PREFS_DEBUG
 // #define WIND_DEBUG
